b210ed6f49b1d7e03969f6c588e993ae47ec460f
pablohdzvizcarra <pablourbanohdzvizcarra@gmail.com>
2026-04-22 03:22:05 +0000

    first commit

---
+++ src/main/java/com/github/pablohdzvizcarra/KernelPulseApp.java
@@ -0,0 +1,10 @@
+package com.github.pablohdzvizcarra;
+
+/**
+ * Hello world!
+ */
+public class KernelPulseApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/github/pablohdzvizcarra/KernelPulseAppTest.java
@@ -0,0 +1,18 @@
+package com.github.pablohdzvizcarra;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class KernelPulseAppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
